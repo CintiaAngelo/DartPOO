8a2cd7ec2b5232a941c342c463aae983c6649722
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-11 19:20:46 -0300

    Primeiro commit

---
+++ bin/cintiac.dart
@@ -0,0 +1,5 @@
+import 'package:cintiac/cintiac.dart' as cintiac;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cintiac.calculate()}!');
+}
---
+++ test/cintiac_test.dart
@@ -0,0 +1,8 @@
+import 'package:cintiac/cintiac.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
